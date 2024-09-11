2ca0ef9c6a0fc50314696fdaa0231a0995f38b5a
Ralf Beckers <Ralf.Beckers@hotmail.com>
2024-09-11 18:12:55 +0200

    Update

--- src/CanonUtility/Usings.cs
+++ src/CanonUtility/Usings.cs
@@ -1,26 +1,18 @@
 ﻿global using System;
+global using System.ComponentModel;
 global using System.Collections.Generic;
 global using System.Globalization;
 global using System.IO;
 global using System.Linq;
-global using System.Text;
-global using System.Threading.Tasks;
-
+global using System.Reflection;
+global using System.Windows;
+global using System.Windows.Controls;
 global using System.Windows.Data;
 global using System.Windows.Input;
 global using System.Windows.Shell;
-global using System.Windows;
-global using System.Runtime.InteropServices;
-global using Microsoft.Xaml.Behaviors;
-
-global using System.Windows.Controls;
-
-global using System.ComponentModel;
-
-global using System.Reflection;
-
-
 global using System.Windows.Threading;
+
 global using CommunityToolkit.Mvvm.ComponentModel;
 global using CommunityToolkit.Mvvm.Input;
+
 global using CanonAPI;
--- src/CanonWpf/Usings.cs
+++ src/CanonWpf/Usings.cs
@@ -1,6 +1,4 @@
-﻿global using System.ComponentModel;
-global using System.Globalization;
-global using System.Reflection;
+﻿global using System.Globalization;
 global using System.Windows;
 global using System.Windows.Controls;
 global using System.Windows.Data;
